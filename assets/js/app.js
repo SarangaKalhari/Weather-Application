6e2b1b1e59238d7d13dd047e4e73dd6eefd2f626
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-16 21:52:51 +0530

    feat(ui): implement complete weather app UI layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,53 @@
 console.log("Js lodded!")
 
 function callApi(){
-    fetch("http://api.weatherapi.com/v1/current.json?key=59918b66dfa542389d3180337260901&q=galle&aqi=no")
+    fetch("http://api.weatherapi.com/v1/forecast.json?key=59918b66dfa542389d3180337260901&q=galle&days=1&aqi=no&alerts=no")
     .then(responce => responce.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        setCurrentDetails(data);
+        let forecast=data.forecast.forecastday[0].hour;
+        console.log(forecast)
+        setHourlyForecast(forecast);
+    })
 }
 
 callApi();
+
+function setCurrentDetails(currentDetails){
+    let temp = document.getElementById("temp_id");
+    let mainStatusImage = document.getElementById("weather_icon");
+    let current =document.getElementById("current_weather");
+    let cityName = document.getElementById("current_location");
+    let date = document.getElementById("date");
+
+    temp.innerText = currentDetails.current.temp_c;
+    mainStatusImage.src = currentDetails.current.condition.icon;
+    current.innerText = currentDetails.current.condition.text;
+    cityName.innerText = currentDetails.location.name+", "+ currentDetails.location.country;
+    date.innerText = currentDate;
+
+}
+
+
+function setHourlyForecast(forecast){
+
+    let time = document.getElementById("hour_time");
+    let img = document.getElementById("hour_status");
+    let temp = document.getElementById("hour_temp");
+    
+    
+    for (let index = 0; index < array.length; index++) {
+    // const element = forecast[index];
+    time.innerText = forecast[index].time;
+    temp.innerText= forecast[index].temp_c;
+    img.src = forecast[index].condition.icon;
+}
+
+
+} 
+
+
 
 let date = new Date();
 
@@ -24,65 +65,7 @@ console.log(currentDate);
 
 
 
-// let searchCity=document.getElementById("txtSearch");
-
-// searchCity.addEventListener("keypress",e=>{
-//     if(e.key=='Enter'){
-//         console.log(searchCity.value);
-//         callApi(searchCity.value.trim());
-//     }
-
-// })
-
-// async function callApi(cityName=""){
-//     // fetch("http://api.weatherapi.com/v1/current.json?key=d774df04809f4737acb70252251308&q=ganemulla&aqi=no")
-//     // .then(responce => responce.json())
-//     // .then(data => console.log(data))
-
-//     fetch(`http://api.weatherapi.com/v1/current.json?key=968ff581242d4e59a6c150445252108&q=${cityName}&aqi=no`)
-//     .then((responce) => responce.json())
-//     .then((data) => {
-//         setDetails(data);
-//     });
-
-//     // let responce=await fetch("http://api.weatherapi.com/v1/current.json?key=d774df04809f4737acb70252251308&q=ganemulla&aqi=no");
-//     // let data=await responce.json();
-//     // console.log(data);
-
-// }
 
 
-// function setDetails(countryDetails){
-//     let cityName=document.getElementById("city_name");
-//     let mainStatusImage=document.getElementById("main_status_image");
-//     let countryName=document.getElementById("country_name");
-//     let temperature=document.getElementById("current_temperature");
-//     let uvMeter=document.getElementById("uv_value");
-//     let humidityMeter=document.getElementById("humidity_value");
-//     let windMeter=document.getElementById("wind_value");
 
-    
-//     cityName.innerText=countryDetails.location.name;
-//     mainStatusImage.src=countryDetails.current.condition.icon
-//     countryName.innerText=countryDetails.location.country;
-//     temperature.innerText = countryDetails.current.temp_c;
-//     uvMeter.innerText=countryDetails.current.uv;
-//     humidityMeter.innerText=countryDetails.current.humidity;
-//     windMeter.innerText=countryDetails.current.wind_kph+"kph";
 
-// }
-
-// callApi();
-
-// //----set date-----
-// let date=new Date();
-// formatDate = {
-//     "weekend":`long`,
-//     "month":`long`,
-//     "year":`numeric`,
-//     "day":`numeric`
-// }
-
-// // let currentDate=date.toLocaleDateString("en-US",formatDate);
-// // document.getElementById("date").innerText=currentDate;
-// //---------
